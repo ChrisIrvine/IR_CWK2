d3f88f54003f2c9d8d53c5d373570258747e5049
Christopher Irvine <christopher@irvinegroup.eu>
2017-11-30 14:12:37 +0000

    Initial work

--- Base Code/indexer.py
+++ Base Code/indexer.py
@@ -6,7 +6,7 @@ import json
 # global declarations for doclist, postings, vocabulary
 docids = []
 postings = {}
-vocab = {}
+vocab = []
 
 # main is used for offline testing only
 def main():
@@ -94,18 +94,68 @@ def clean_html(page_contents):
     cleantext = re.sub('([\W]+)', ' ', cleantext)
     return cleantext
 
+# Function:     make_index(url, page_contents)
+# Parameters:   String - url (the url of the page being scraped by PCcrawler.py
+#               String - page_content (source code of the scraped webpage)
+# Purpose:      Process the source code from a website, that is cleaned by
+#               clean_text(page_contents), and creates index tables for future
+#               retrieval.
+# Returns:      No inherit returns, but outputs content into:
+#                   - docids.txt
+#                   - postings.txt
+#                   - vocab.txt
 def make_index(url, page_contents):
-    for url in postings.keys():
-        for word in postings[url].keys():
-            if word in postings.keys():
-                if url in postings[word].keys():
-                    postings[word][url].extend(vocab[url][word][:])
-                else:
-                    postings[word][url] = vocab[url][word]
-            else:
-                postings[word] = {url: vocab[url][word]}
-    return
+    # declare refs to global variables
+    global docids
+    global postings
+    global vocab
 
+    # first convert bytes to string if necessary
+    if isinstance(page_contents, bytes):
+        page_contents = page_contents.decode('utf-8', 'ignore')
+
+    print('===============================================')
+    print('make_index: url = ', url)
+    print('===============================================')
+
+    # Send the contents of the scraped page to be cleaned, store output in page_text
+    page_text = clean_html(page_contents)
+
+    # This code runs for each URL, therefore important not to overwrite
+    # existing data.
+
+    # Store the URLs that are scraped in the docids file
+    docids.append(url)
+    # Using list comprehension, create the tokens list:
+    # - split page_text by spaces
+    # - take each token in the separated page_text
+    # - lower case each token
+    # - store the lower case token in tokens
+    tokens = [t.lower() for t in page_text.split()]
+
+    # Using list comprehension; create the vocabulary list, to act like a set:
+    # - for every token in tokens
+    # - if token does not exist within vocab
+    #   - add token into vocab
+    # - else
+    #   - move onto next token
+    # - check the token length is greater than 1
+    vocab.extend([t for t in tokens if t not in vocab if len(t)>1])
+
+    # for each token in vocabulary, get the tokenID (using enumerate)
+    for tokenID, token in enumerate(vocab):
+        # if that tokenID does not exist within postings
+        if tokenID not in postings:
+            # create an empty list at that tokenID within postings
+            postings[tokenID] = []
+        # count the frequency of a token within tokens
+        freq = tokens.count(token)
+        # check that the token occurs at least once
+        if freq != 0:
+            # append that the occurrence of that token to the correct tokenID
+            # entry in postings, with the relevant url source
+            postings[tokenID].append([docids.index(url), tokens.count(token)])
+    return
 
 # Standard boilerplate to call the main() function to begin
 # the program.
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # IR_CWK2 #
+
+# ToDo
+- Rework indexing to include tf*idf ranking
+- Create a boolean retrieval system to work in tandem with tf*idf
+- Extra pizaz
